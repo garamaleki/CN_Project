76bc7507bae0d749019eeaa5d3b1e1f2f528400a
amirMalek <amirhossein.maleki@ee.sharif.edu>
2019-01-19 17:44:09 +0330

    Modify Node.py

--- src/tools/Node.py
+++ src/tools/Node.py
@@ -28,7 +28,7 @@ class Node:
             mode = "public"
         elif (self.server_ip == Node.parse_ip("127.0.0.1")):
             mode = "localhost"
-        self.client = ClientSocket()
+        self.client = None
         try:
             self.client = ClientSocket(mode,self.server_port,2048,False)
         except:
